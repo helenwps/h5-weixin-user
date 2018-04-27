b9c3b1d53c18b0223d2337462f75b69feab528b2
ÂÉʦ¶Ë-ǰ¶Ë-ÎâÅåɺ952 <709369680@qq.com>
2018-04-27 09:47:26 +0800

    代码提交

---
+++ src/api/userDatum.api.js
@@ -0,0 +1,34 @@
+import Ajax from 'lib/Ajax';
+
+//案例详情
+export const userCenter = {
+	userInfo(query) {
+		return Ajax({
+			url: '/user/user-info',
+			method: 'get'
+		});
+	},
+	editUserInfo(param){
+		return Ajax({
+			url: '/user/user-info',
+			method: 'post',
+			data:param
+		});
+	}
+};
+
+//我的预约
+export const subscribe = {
+	list(query){
+		return Ajax({
+			url:'/wx/api/demand/list',
+			method:'get'
+		});
+	},
+	info(query){
+		return Ajax({
+			url:`/wx/api/demand/${query.uuid}`,
+			method:'get'
+		})
+	}
+}
